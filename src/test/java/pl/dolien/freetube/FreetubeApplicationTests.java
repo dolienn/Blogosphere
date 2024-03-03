5b6f100dec643d3c4daa181fd86245994d192597
dolienn <thedolien@gmail.com>
2024-03-03 18:38:10 +0100

    Initial commit

---
+++ src/test/java/pl/dolien/freetube/FreetubeApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.dolien.freetube;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreetubeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
