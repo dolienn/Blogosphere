189fb3a8df8a343f70be0c2b34707f36473237ea
dolienn <thedolien@gmail.com>
2024-03-09 11:52:18 +0100

    Login and registration pages and system

--- src/main/java/pl/dolien/freetube/security/DemoSecurityConfig.java
+++ src/main/java/pl/dolien/freetube/security/DemoSecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
@@ -46,7 +47,7 @@ public class DemoSecurityConfig {
                                 .successHandler(customAuthenticationSuccessHandler)
                                 .permitAll()
                 )
-                .logout(logout -> logout.permitAll()
+                .logout(LogoutConfigurer::permitAll
                 )
                 .exceptionHandling(configurer ->
                         configurer.accessDeniedPage("/access-denied")
